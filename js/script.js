b6e9692c9fc25e6c2468db654f996976a43d04b8
Стас <TimeToAgain@tutamail.com>
2025-04-13 02:24:46 +0500

    создание бургер кнопки

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-const avionHome = document.getElementsById('logo');
- avionHome.addEventListener('click', function() {
-    window.location.href = 'index.html';
- });
+   const burger = document.getElementById('burger');
+   const menu = document.getElementById('header_menu');
+   
+   function toggleMenu() {
+      menu.classList.toggle('show');
+      burger.classList.toggle('active');
+   }
+   
+   if (burger) {
+      burger.addEventListener('click', function(event) {
+         event.stopPropagation();
+         toggleMenu();
+      });
+   }
+   
+   document.addEventListener('click', function(event) {
+      if (menu && menu.classList.contains('show') && !menu.contains(event.target) && !burger.contains(event.target)) {
+         menu.classList.remove('show');
+         burger.classList.remove('active');
+      }
+   });
